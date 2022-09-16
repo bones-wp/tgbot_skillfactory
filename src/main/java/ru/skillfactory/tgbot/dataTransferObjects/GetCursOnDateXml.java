e7bf5916f3f8d5a4a8020c70805815321eab3e32
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-09-16 17:00:17 +0300

    2

--- src/main/java/ru/skillfactory/tgbot/dataTransferObjects/GetCursOnDateXml.java
+++ src/main/java/ru/skillfactory/tgbot/dataTransferObjects/GetCursOnDateXml.java
@@ -7,8 +7,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 @Data
-@XmlRootElement(name = "GetCursOnDateXML", namespace = "https://web.cbr.ru/")
+@XmlRootElement(name = "GetCursOnDateXML", namespace = "http://web.cbr.ru/")
 public class GetCursOnDateXml {
-    @XmlElement(name = "On_Date", required = true, namespace = "https://web.cbr.ru/") //Указание на то, в каком теге XML должно быть данное поле
+    @XmlElement(name = "On_Date", required = true, namespace = "http://web.cbr.ru/") //Указание на то, в каком теге XML должно быть данное поле
     protected XMLGregorianCalendar onDate;
 }
--- src/main/java/ru/skillfactory/tgbot/dataTransferObjects/GetCursOnDateXmlResponse.java
+++ src/main/java/ru/skillfactory/tgbot/dataTransferObjects/GetCursOnDateXmlResponse.java
@@ -7,11 +7,11 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "GetCursOnDateXMLResponse", namespace = "https://web.cbr.ru/") //Указываем XML тег и пространство имен
+@XmlRootElement(name = "GetCursOnDateXMLResponse", namespace = "http://web.cbr.ru/") //Указываем XML тег и пространство имен
 @XmlAccessorType(XmlAccessType.FIELD) //Указываем, как получить/записать значения в поля данного объекта
 @Data //Генерируем геттеры и сеттеры
 public class GetCursOnDateXmlResponse {
 
-    @XmlElement(name = "GetCursOnDateXMLResult", namespace = "https://web.cbr.ru/") //Имя xml элемента и его пространство имен
+    @XmlElement(name = "GetCursOnDateXMLResult", namespace = "http://web.cbr.ru/") //Имя xml элемента и его пространство имен
     private GetCursOnDateXmlResult getCursOnDateXmlResult;
 }
