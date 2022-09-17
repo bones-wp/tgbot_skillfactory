9f3f0e29f922bfb76740c1789dccf9bb286c99a8
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-09-17 16:30:28 +0300

    App - created OK

--- src/main/java/ru/skillfactory/tgbot/dataTransferObjects/GetCursOnDateXml.java
+++ src/main/java/ru/skillfactory/tgbot/dataTransferObjects/GetCursOnDateXml.java
@@ -2,13 +2,18 @@ package ru.skillfactory.tgbot.dataTransferObjects;
 
 import lombok.Data;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.datatype.XMLGregorianCalendar;
 
-@Data
 @XmlRootElement(name = "GetCursOnDateXML", namespace = "http://web.cbr.ru/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data
 public class GetCursOnDateXml {
-    @XmlElement(name = "On_Date", required = true, namespace = "http://web.cbr.ru/") //Указание на то, в каком теге XML должно быть данное поле
+
+    @XmlElement(name = "On_date", required = true, namespace = "http://web.cbr.ru/")
     protected XMLGregorianCalendar onDate;
+
 }
